be29bdc0dcfde15a5bb735211ded8e3340c13341
Xerax <alexreed@outlook.com>
2013-03-03 15:37:50 +0000

    Began work on the Play page.

---
+++ src/GMusic.WP.8/Pages/Authorized/Play.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace GMusic.WP._8.Pages.Authorized
+{
+	public partial class Play : PhoneApplicationPage
+	{
+		public Play()
+		{
+			InitializeComponent();
+		}
+	}
+}
